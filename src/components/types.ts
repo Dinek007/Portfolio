afce2a40629e5b6e735b901684d9d6b9a025b121
dinek <wilk.dinek@gmail.com>
2020-07-25 17:52:18 +0200

    add mySkills component

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,2 +1,8 @@
 export type HeaderButtonComponentProps = { text: string }
+export type SkillElementComponentProps = {
+    primary?: string,
+    secondary?: string,
+    ico?: string,
+    xs?: number
+}
 
