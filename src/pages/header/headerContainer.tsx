b49f6674a65652922dde43228d58e0cad04c8ffe
dinek <wilk.dinek@gmail.com>
2020-07-26 21:07:28 +0200

    header improve

--- src/pages/header/headerContainer.tsx
+++ src/pages/header/headerContainer.tsx
@@ -1,10 +1,21 @@
 import React from "react"
 
 import { HeaderComponent } from "./headerComponent"
-import { HeaderContainerProps } from "./types";
+import { HeaderContainerProps, ItemsData } from "./types";
 
+const buttonsNames = ["About me", "My skills", "My projekts", "Contact"]
+const itemsData: ItemsData = {
+    description: "PORTFOLIO PAGE",
+    title: "DAMIAN WILK",
+    picture: "../../../img/matrix.png"
+}
 export const HeaderContainer: React.FC<HeaderContainerProps> = () => {
+    const buttons = buttonsNames
+    const items = itemsData
     return (
-        <HeaderComponent></HeaderComponent>
+        <HeaderComponent
+            buttons={buttons}
+            items={items}
+        />
     )
 }
--- src/pages/header/types.ts
+++ src/pages/header/types.ts
@@ -1,2 +1,14 @@
-export type HeaderComponentProps = {}
-export type HeaderContainerProps = {}
+export type HeaderComponentProps = {
+    buttons: string[];
+    items: ItemsData;
+}
+export type HeaderContainerProps = {
+    buttons?: string[];
+    items?: ItemsData;
+}
+
+export type ItemsData = {
+    description: string;
+    title: string;
+    picture: string;
+}
